5353968bdca82f418eafef55d00ad0295144033a
Qianxing Li <qianxing.li@stonybrook.edu>
2024-03-25 12:59:19 -0400

    Update payment status values

--- frontend/src/wallet/columns.tsx
+++ frontend/src/wallet/columns.tsx
@@ -20,11 +20,11 @@ export const columns: ColumnDef<IPayment>[] = [
     cell: ({ row }) => {
       const status = row.getValue("status") as string;
       const color =
-        status === "pending"
+        status === "Pending"
           ? "text-yellow-500"
-          : status === "processing"
+          : status === "Processing"
           ? "text-blue-500"
-          : status === "success"
+          : status === "Success"
           ? "text-green-500"
           : "text-red-500";
       return (
